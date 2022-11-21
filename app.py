6fc99870e58619369611451a80d9b0302b663325
torkfat <105743305+torkfat@users.noreply.github.com>
2022-11-21 17:05:59 -0500

    Doesn't Work Yet

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template, redirect, request, session
+import sqlite3
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
