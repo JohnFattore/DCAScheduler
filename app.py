5ff9f72db20420302addb6e11e16f8cbed429442
torkfat <105743305+torkfat@users.noreply.github.com>
2022-11-21 21:39:26 -0500

    it works!

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_session import Session
 
 app = Flask(__name__)
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
